23c784dc59db3a33262f97f70ef1a881a5f1f8c9
Alek Ortiz <aleksebastiano@gmail.com>
2021-01-22 16:47:51 -0600

    Commented out code in client folder that's irrelevant to this feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,37 +5,37 @@ class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      productId: 1,
-      primaryPhotoUrl: null
+      // productId: 1,
+      // primaryPhotoUrl: null
     };
   }
 
   componentDidMount() {
-    fetch(`http://localhost:4002/photos/product/${this.state.productId}/primary`)
-    .then(res => res.json())
-    .then((primaryPhotoUrl) => this.setState({
-      primaryPhotoUrl: primaryPhotoUrl
-    })
-    );
+    // fetch(`http://localhost:4002/photos/product/${this.state.productId}/primary`)
+    // .then(res => res.json())
+    // .then((primaryPhotoUrl) => this.setState({
+    //   primaryPhotoUrl: primaryPhotoUrl
+    // })
+    // );
 
-    fetch(`http://localhost:4002/photos/id/${this.state.productId}`)
-    .then(res => res.json())
-    .then((allProducts) => console.log(allProducts));
+    // fetch(`http://localhost:4002/photos/id/${this.state.productId}`)
+    // .then(res => res.json())
+    // .then((allProducts) => console.log(allProducts));
 
-    fetch(`http://localhost:4002/photos/features/${this.state.productId}`)
-    .then(res => res.json())
-    .then((featuresPhotosUrls) => console.log(featuresPhotosUrls));
+    // fetch(`http://localhost:4002/photos/features/${this.state.productId}`)
+    // .then(res => res.json())
+    // .then((featuresPhotosUrls) => console.log(featuresPhotosUrls));
 
-    let itemsToFetch = [1, 5, 6, 99, 100];
-    fetch(`http://localhost:4002/photos/product/primary/multiple`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(itemsToFetch)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data));
+    // let itemsToFetch = [1, 5, 6, 99, 100];
+    // fetch(`http://localhost:4002/photos/product/primary/multiple`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(itemsToFetch)
+    // })
+    // .then(res => res.json())
+    // .then(data => console.log(data));
   }
 
   render () {
@@ -44,7 +44,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Here's the product's primary photo</h1>
-        <img src={this.state.primaryPhotoUrl}></img>
+        {/* <img src={this.state.primaryPhotoUrl}></img> */}
       </div>
     );
   }
