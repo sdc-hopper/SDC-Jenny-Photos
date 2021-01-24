aa480baad76ee3e4617dd9df5654174e67b20c61
Alek Ortiz <aleksebastiano@gmail.com>
2021-01-24 14:29:50 -0600

    Implemented db seeding and API tests

--- server/index.js
+++ server/index.js
@@ -12,25 +12,44 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.get('/photos/id/:productId', (req, res) => {
   let productId = req.params.productId;
   dbQuery.getAllProductPhotos(productId)
-  .then(productPhotoUrls => res.status(200).send(JSON.stringify(productPhotoUrls)));
+  .then(productPhotoUrls => {
+    console.log(productPhotoUrls);
+    if (!productPhotoUrls) {
+      res.status(404).send('Invalid product id');
+    } else {
+      res.status(200).send(JSON.stringify(productPhotoUrls));
+    }
+  });
 });
 
 app.get('/photos/product/:productId/primary', (req, res) => {
   let productId = req.params.productId;
   dbQuery.getProductPrimaryPhoto(productId)
-  .then(primaryPhotoUrl => res.status(200).send(JSON.stringify(primaryPhotoUrl)));
+  .then(primaryPhotoUrl => {
+    if (!primaryPhotoUrl) {
+      res.status(404).send('Invalid product id');
+    } else {
+      res.status(200).send(JSON.stringify(primaryPhotoUrl));
+    }
+  });
 });
 
 app.get('/photos/features/:productId', (req, res) => {
   let productId = req.params.productId;
   dbQuery.getProductFeaturesPhotos(productId)
-  .then(featuresPhotosUrls => res.status(200).send(JSON.stringify(featuresPhotosUrls)));
+  .then(featuresPhotosUrls => {
+    if (!featuresPhotosUrls) {
+      res.status(404).send('Invalid product id');
+    } else {
+      res.status(200).send(JSON.stringify(featuresPhotosUrls));
+    }
+  });
 });
 
 app.post('/photos/product/primary/multiple', (req, res) => {
   let productIds = req.body;
   if (productIds.length > 30 || productIds.length === 0 || !productIds) {
-    res.status(400);
+    res.status(400).send('Request limit is 30 product ids');
   } else {
     dbQuery.getMultipleProductsPrimaryPhotos(productIds)
     .then(primaryPhotoUrls => res.status(200).send(JSON.stringify(primaryPhotoUrls)));
@@ -39,3 +58,5 @@ app.post('/photos/product/primary/multiple', (req, res) => {
 
 
 app.listen(port, () => console.log(`listening on port ${port}`));
+
+module.exports = app;
--- test/apiSpec.js
+++ test/apiSpec.js
@@ -12,59 +12,59 @@ describe('API calls', () => {
       requester = await chai.request(app).keepOpen();
     });
 
-    // describe('Single product API call - GET /priceandinventory/id/:productId', () => {
-    //   // valid ids are 1000-1099
-    //   const id = 1000;
-    //   const invalidId = 100;
+    describe('Single product API call - GET /photos/id/:productId', () => {
+      // valid ids are 1000-1099
+      const id = 1000;
+      const invalidId = 100;
 
-    //   it('should get a product\'s price and inventory count', (done) => {
-    //     requester.get(`/priceandinventory/id/${id}`)
-    //       .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.body.should.be.a('object');
-    //         done();
-    //       });
-    //   });
+      it('should get all pictures related to a product\'s id', (done) => {
+        requester.get(`/photos/id/${id}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+          });
+      });
 
-    //   it('should respond with a 400 status code when requesting an invalid product id', (done) => {
-    //     requester.get(`/priceandinventory/id/${invalidId}`)
-    //       .end((err, res) => {
-    //         res.should.have.status(404);
-    //         done();
-    //       });
-    //   });
-    // });
+      it('should respond with a 400 status code when requesting an invalid product id', (done) => {
+        requester.get(`/photos/id/${invalidId}`)
+          .end((err, res) => {
+            res.should.have.status(404);
+            done();
+          });
+      });
+    });
 
-    // describe('Multiple products API call - POST /priceandinventory/id/multiple', () => {
-    //   // valid ids are 1000-1099
-    //   const ids = [1000, 1005, 1010, 1085];
+    describe('Multiple products API call - POST /photos/product/primary/multiple', () => {
+      // valid ids are 1000-1099
+      const ids = [1000, 1005, 1010, 1085];
 
-    //   it('should get multiple products\' prices and inventory counts', (done) => {
-    //     requester.post('/priceandinventory/id/multiple')
-    //       .send(ids)
-    //       .end((err, res) => {
-    //         res.should.have.status(200);
-    //         res.body.should.be.a('object');
-    //         done();
-    //       });
-    //   });
+      it('should get multiple products\' primary photo urls', (done) => {
+        requester.post('/photos/product/primary/multiple')
+          .send(ids)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+          });
+      });
 
-    //   it('should respond with a 404 status code when requesting too many products', (done) => {
-    //     let requestLimit = 30;
-    //     let tooManyProductIds = [];
-    //     // push 31 valid ids into an array to send with the request
-    //     for (let i = 0; i <= requestLimit; i++) {
-    //       tooManyProductIds.push(i + 1000);
-    //     };
+      it('should respond with a 404 status code when requesting too many product ids', (done) => {
+        let requestLimit = 30;
+        let tooManyProductIds = [];
+        // push 31 valid ids into an array to send with the request
+        for (let i = 0; i <= requestLimit; i++) {
+          tooManyProductIds.push(i + 1000);
+        };
 
-    //     requester.post('/priceandinventory/id/multiple')
-    //       .send(tooManyProductIds)
-    //       .end((err, res) => {
-    //         res.should.have.status(400);
-    //         done();
-    //       });
-    //   });
-    // });
+        requester.post('/photos/product/primary/multiple')
+          .send(tooManyProductIds)
+          .end((err, res) => {
+            res.should.have.status(400);
+            done();
+          });
+      });
+    });
 
     after(async () => {
       await requester.close();
