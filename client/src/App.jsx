60113a214e4ed918be4fb6a8a307ba68c57ed8dc
Alek Ortiz <aleksebastiano@gmail.com>
2021-02-20 14:36:44 -0600

    Finished photos modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,6 +39,7 @@ class Photos extends React.Component {
     this.setPrimary = this.setPrimary.bind(this);
     this.setCoordinates = this.setCoordinates.bind(this);
     this.toggleZoom = this.toggleZoom.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   setPrimary(e) {
@@ -62,6 +63,12 @@ class Photos extends React.Component {
   toggleZoom() {
     this.setState({
       zoom: !this.state.zoom
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      modal: !this.state.modal
     });
   }
 
@@ -90,15 +97,24 @@ class Photos extends React.Component {
       popover = null;
     }
 
+    const modalState = this.state.modal;
+    let modal;
+    if (modalState) {
+      modal = <PhotosModal
+      productId={this.state.productId}
+      primaryPhotoUrl={this.state.primaryPhotoUrl}
+      productInfo={this.state.productInfo}
+      setPrimary={this.setPrimary}
+      photos={this.state.productPhotosUrls}
+      toggleModal={this.toggleModal}
+      />
+    } else {
+      modal = null;
+    }
+
   return (
     <div>
-      <PhotosModal
-        productId={this.state.productId}
-        primaryPhotoUrl={this.state.primaryPhotoUrl}
-        productInfo={this.state.productInfo}
-        setPrimary={this.setPrimary}
-        photos={this.state.productPhotosUrls}
-      />
+      {modal}
       <PhotosWrapper>
         <Thumbnails
           flexDirection={"column"}
@@ -110,6 +126,7 @@ class Photos extends React.Component {
           <PrimaryPhoto
             onMouseEnter={() => this.toggleZoom()}
             onMouseLeave={() => this.toggleZoom()}
+            onClick={() => this.toggleModal()}
             onMouseMove={(e) => this.setCoordinates(e)}
             style={{maxWidth: "100%", height: "auto"}} src={this.state.primaryPhotoUrl}>
           </PrimaryPhoto>
--- client/src/components/PhotosModal.jsx
+++ client/src/components/PhotosModal.jsx
@@ -16,6 +16,28 @@ const PhotosModalWrapper = styled.div`
     width: 98.5%;
     height: 95%;
   };
+
+  -webkit-animation: fadein 0.5s;
+  -moz-animation: fadein 0.5s;
+  -ms-animation: fadein 0.5s;
+  -o-animation: fadein 0.5s;
+  animation: fadein 0.5s;
+
+
+  @keyframes fadein {
+      from { opacity: 0; }
+      to   { opacity: 1; }
+  }
+
+  @-moz-keyframes fadein {
+      from { opacity: 0; }
+      to   { opacity: 1; }
+  }
+
+  @-webkit-keyframes fadein {
+      from { opacity: 0; }
+      to   { opacity: 1; }
+  }
 `;
 
 const PhotosModalEl = styled.div`
@@ -32,13 +54,31 @@ const PrimaryPhotoWrapper = styled.div`
 const PrimaryPhoto = styled.img`
   max-width: 100%;
   height: auto;
+  @media (max-width: 1400px) {
+    max-width: 90%;
+  };
 `;
 
 const ProductInfoWrapper = styled.div`
-
+  padding-top: 5%;
+  align-self: flex-start;
 `;
 const ProductInfo = styled.div`
+`;
 
+const ProductTitle = styled.span`
+  display: block;
+  font-family: verdana,arial,helvetica,sans-serif;
+  font-size: 17px;
+  margin-bottom: 2em;
+`;
+
+const ProductColorAndConfiguration = styled.span`
+  display: block;
+  font-family: verdana,arial,helvetica,sans-serif;
+  font-size: 12px;
+  color: #767676;
+  margin-bottom: 1.5em;
 `;
 
 const X = styled.span`
@@ -54,7 +94,7 @@ const PhotosModal = (props) => {
   let [openModal, setModal] = useState(false);
   return (
     <PhotosModalWrapper>
-      <X>x</X>
+      <X onClick={() => props.toggleModal()}>x</X>
       <PhotosModalEl>
         <PrimaryPhotoWrapper>
           <PrimaryPhoto
@@ -62,7 +102,11 @@ const PhotosModal = (props) => {
           </PrimaryPhoto>
         </PrimaryPhotoWrapper>
         <ProductInfoWrapper>
-          <ProductInfo>{props.productInfo.name}</ProductInfo>
+          <ProductInfo>
+            <ProductTitle>{props.productInfo.name}</ProductTitle>
+            <ProductColorAndConfiguration>Color: {props.productInfo.color}</ProductColorAndConfiguration>
+            <ProductColorAndConfiguration>Configuration: {props.productInfo.configuration}</ProductColorAndConfiguration>
+          </ProductInfo>
           <Thumbnails
             setPrimary={props.setPrimary}
             primaryPhotoUrl={props.primaryPhotoUrl}
