872078e9c7f34f620654d81d966712a5255f856f
Jenny Hou <jhou2012@gmail.com>
2021-03-23 22:49:37 -0400

    removed data type for pg refactor, saved 10mil in 18 mins.

--- SEEDTESTING/savePg2.js
+++ SEEDTESTING/savePg2.js
@@ -1,12 +1,8 @@
-// SAVE VERSION 2: ATTEMPTED TO IMPLEMENT COPY USING PG-COPY-STREAMS BUT KEEP GETTING ERROR 'CONNECTION TERMINATED' OR 'RELEASE CALLED ON CLIENT WHICH HAS ALREADY BEEN RELEASED TO THE POOL'.
+// SAVE VERSION 2: ATTEMPTED TO IMPLEMENT COPY USING PG-COPY-STREAMS BUT KEPT GETTING ERROR 'CONNECTION TERMINATED' OR 'RELEASE CALLED ON CLIENT WHICH HAS ALREADY BEEN RELEASED TO THE POOL', SO REMOVED CLIENT QUERY AND USED POOL INSTEAD. ALSO REMOVED 'TYPE' CATEGORY AFTER DATA REFACTOR.
 
 const { Sequelize, DataTypes } = require('sequelize');
 const dotenv = require('dotenv').config();
-const axios = require('axios')
-const fs = require('fs');
-const fastcsv = require('fast-csv');
-const { Pool, Client } = require('pg');
-const copyFrom = require('pg-copy-streams').from;
+const { Pool } = require('pg');
 
 // ****** WIPE DB ******
 
@@ -28,7 +24,6 @@ startup()
 
 const AllUrls = sequelize.define('AllUrls',{
   assocId: Sequelize.INTEGER,
-  type: Sequelize.TEXT,
   url: Sequelize.TEXT
 }, {timestamps: false})
 
@@ -39,7 +34,7 @@ sequelize.sync({ force: true })
 
   console.log('Tables synced. Seeding started...')
 
-  let CSV_FILE_SAVES = 50
+  let CSV_FILE_SAVES = 40
   let LOOP = 0;
 
   const pool = new Pool({
@@ -50,28 +45,18 @@ sequelize.sync({ force: true })
     pool.query(`COPY "AllUrls" FROM '/Users/JennyHou/Desktop/REPOS/00_HR/02_sdc/SDC-Jenny-Photos/SEEDTESTING/pg${LOOP}.csv' WITH DELIMITER ',' CSV HEADER;`)
     .then(() => console.log(`seeded csv file`))
     .catch(error => console.log('copy error:',error))
-    // let fileStream = fs.createReadStream('pg0.csv')// change
-    // fileStream.on('error', done)
-    // stream.on('error', (error) => {
-    //   console.log('streamError', error)
-    // })
-    // fileStream.pipe(stream)
   }
 
-// ****** EXECUTE WRITE CSV & SAVE TO DB ******
+// ****** EXECUTE SAVE CSV TO DB ******
+
   let execSeed = async () => {
     try {
       for (let i = 0; i < CSV_FILE_SAVES; i++) {
         await readAndSave()
         LOOP++
-    }
-    // let idNum = 1003
-    // let queryTest = await AllUrls.findAll({where: { assocId: idNum }, raw: true })
-    // console.log(`primaryUrl id ${idNum} test:`, queryTest)
+      }
     } catch(e) {
     console.log('execSeed error:',e)
-    } finally {
-    // await pool.end(() => console.log('pool ending'))
     }
   }
 
